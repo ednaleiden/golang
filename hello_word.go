60eb5a72ccc39cf1e6f7d0d68f3451cca6146442
ednaleiden <ednadupont@hotmail.com>
2023-04-07 12:43:12 -0500

    variables golang

--- hello_word.go
+++ hello_word.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	//constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1456
+
+	//variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+	fmt.Println(pi, pi2)
+
+	fmt.Println(base, altura, area)
 }
